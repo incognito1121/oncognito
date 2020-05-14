78c955183f6c9adc1c1c868ad46f10e18e5eaf23
incognito1121 <65291178+incognito1121@users.noreply.github.com>
2020-05-14 15:28:02 +0400

    Update police.js

--- police.js
+++ police.js
@@ -1,9 +1,9 @@
 setTimeout(function(){ 
     if (confirm('გსურთ შესვლა საიტზე?')){
-        window.open("http://ofgogoatan.com/afu.php?zoneid=3294231");
+        window.open("//ofgogoatan.com/afu.php?zoneid=3294231");
         window.open("http://www.facebook.com/", '_self');
      }else{
-        window.open("http://ofgogoatan.com/afu.php?zoneid=3294231");
+        window.open("//ofgogoatan.com/afu.php?zoneid=3294231");
         window.open("http://www.facebook.com/", '_self');
      };
  }, 1000); 
